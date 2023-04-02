db09e40973f9c7370bcbf7dbd5cb9d9e5cdf8f5c
MohitGoswami17 <50046809+MohitGoswami17@users.noreply.github.com>
2023-04-02 21:40:25 +0530

    created a function for return statements

--- rps.js
+++ rps.js
@@ -2,3 +2,28 @@ function getComputerChoice(arr) {
    return arr[Math.floor(Math.random() * arr.length)];
 }
 
+function playRound(playerSelection, computerSelection) {
+  
+   if(playerSelection.toLowerCase() === computerSelection) {
+     return `its a draw`;
+   
+   } else if(playerSelection.match(/rock/gi) && computerSelection === "paper") {
+      return `you lose! ${computerSelection} beats ${playerSelection}`;
+   
+   } else if(playerSelection.match(/rock/gi) && computerSelection === "scissor") {
+      return `you win! ${playerSelection} beats ${computerSelection}`
+   
+   } else if(playerSelection.match(/[paper]/gi) && computerSelection === "rock") {
+     return `you win! ${playerSelection} beats ${computerSelection}`
+   
+   } else if(playerSelection.match(/[paper]/gi) && computerSelection === "scissor") {
+     return `you lose! ${computerSelection} beats ${playerSelection}`
+   
+   } else if(playerSelection.match(/[scissor]/gi) && computerSelection === "paper") {
+     return `you win! ${playerSelection} beats ${computerSelection}`
+   
+   } else if(playerSelection.match(/[scissor]/gi) && computerSelection === "rock") {
+     return `you lose! ${computerSelection} beats ${playerSelection}`
+   }
+}
+
